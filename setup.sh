d4d4e4e3929af5bc91fc8b556729e818940d9945
gonzal0lguin <golguinm@gmail.com>
2023-12-05 01:33:39 -0300

    Create sh script to source all local variables

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+echo "Setting up monocular-mapping repository..."
+
+source ros/dev_ws/devel/setup.zsh
+
+export HUSARION_ROS_BUILD_TYPE=simulation
+
+echo "Done configuring monocular-mapping repository."
